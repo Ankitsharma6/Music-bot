0d61ee508d0b6d0253db8e391cc45896d33e20d4
Ankit Sharma <107745030+Ankitsharma6@users.noreply.github.com>
2026-05-31 20:15:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     app: {
-        token: process.env.DISCORD_TOKEN || 'xxx',
+        token: process.env.DISCORD_TOKEN || 'MTUwMjUzMTQyNDAwNjMwNzk1MQ.GOZC1A.CxVVjnMoCjLRzN3de54IQDa9_QNGOe7JgnhLJk',
         playing: 'by the Community ❤️',
         global: true,
-        guild: process.env.GUILD_ID || 'xxx',
+        guild: process.env.GUILD_ID || '1447493918148264080',
         extraMessages: false,
         loopMessage: false,
         lang: 'en',
